e9b25e5b96f3ac692a6518d3550516a1478a71c2
mash0510 <mnbsby@gmail.com>
2016-06-08 13:43:59 +0900

    設定画面の追加

---
+++ DocSearch/Controllers/SetupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocSearch.Controllers
+{
+    public class SetupController : Controller
+    {
+        // GET: Setup
+        public ActionResult Setup()
+        {
+            return View();
+        }
+    }
+}
---
+++ DocSearch/Models/SetupModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocSearch.Models
+{
+    /// <summary>
+    /// 設定画面のモデル
+    /// </summary>
+    public class SetupModel
+    {
+        /// <summary>
+        /// クロール先のフォルダ
+        /// </summary>
+        public string CrawlFolders { set; get; }
+
+    }
+}
